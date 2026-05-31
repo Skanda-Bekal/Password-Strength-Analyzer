fea559dc929dfd8ec7f8c08b1c26a24ca7f0ffc0
Skanda Bekal <skandabekal@gmail.com>
2026-05-31 13:20:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+console.log(`Server running on http://localhost:${PORT}`);
+
+});
